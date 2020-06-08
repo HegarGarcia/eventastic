768052e5ab23f1ac59d07d9db7b7dd9f8a891cfd
Hegar Garcia <hegargarcia@gmail.com>
2020-06-08 15:22:46 -0500

    Fixing eslint problems found in files

--- web/src/constants/firebaseConfig.js
+++ web/src/constants/firebaseConfig.js
@@ -6,5 +6,5 @@ export default {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
