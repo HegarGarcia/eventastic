f054d6a6ecaf1e87331ee045951d13f507d89541
Hegar Garcia <hegargarcia@gmail.com>
2020-06-09 14:23:26 -0500

    Adding test placeholders for Auth Context

---
+++ web/src/context/__test__/auth.test.jsx
@@ -0,0 +1,15 @@
+describe('<AuthContext /> spec', () => {
+  it.todo('Should login with email and password');
+
+  it.todo('Should log in with google');
+
+  it.todo('Should sign out user');
+
+  describe('Sign up with email and password', () => {
+    it.todo('Should sign up with email and password');
+
+    it.todo('Should send email verification');
+
+    it.todo('Should sign out the user after registration');
+  });
+});
