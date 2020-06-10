53686a87bd1d7f260afc3883c9cb57fba506cae5
Hegar Garcia <hegargarcia@gmail.com>
2020-06-10 16:27:13 -0500

    Adding Layout to routing

--- web/src/router/Router.jsx
+++ web/src/router/Router.jsx
@@ -2,17 +2,20 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import PublicRoute from '../components/PublicRoute';
 import ContextProvider from '../context';
+import Layout from '../layout/Layout';
 
 const HomePage = lazy(() => import('../pages/Home'));
 
 const Router = () => (
   <BrowserRouter>
     <ContextProvider>
-      <Suspense fallback="">
-        <Switch>
-          <PublicRoute to="/" component={HomePage} />
-        </Switch>
-      </Suspense>
+      <Layout>
+        <Suspense fallback="">
+          <Switch>
+            <PublicRoute to="/" component={HomePage} />
+          </Switch>
+        </Suspense>
+      </Layout>
     </ContextProvider>
   </BrowserRouter>
 );
--- web/src/styles/GlobalStyles.js
+++ web/src/styles/GlobalStyles.js
@@ -4,6 +4,8 @@ const GloablStyles = createGlobalStyle`
   html, body{
     width: 100vw;
     height: 100vh;
+    padding: 0;
+    margin: 0;
   }
 
   * {
